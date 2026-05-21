1e76511b3dc263ad4ea5d15f817aa326630f5091
Samuel Malta <samuel.agendaos@gmail.com>
2026-05-21 02:50:23 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,6 +11,29 @@ def home():
 
 @app.get("/protocolos")
 def listar_protocolos():
-    response = supabase.table("protocolos").select("*").execute()
+    response = supabase.table("protocolos").select(
+        "id, numero_protocolo, orgao, responsavel, atividade, status_atual, situacao, ativo"
+    ).execute()
+
+    return response.data
+
+
+@app.get("/projetos")
+def listar_projetos():
+    response = supabase.table("projetos").select("*").execute()
+
+    return response.data
+
+
+@app.get("/empresas")
+def listar_empresas():
+    response = supabase.table("empresas").select("*").execute()
+
+    return response.data
+
+
+@app.get("/historico")
+def listar_historico():
+    response = supabase.table("historico_consultas").select("*").execute()
 
     return response.data
