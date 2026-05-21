dbfc4ecb345470fdbf5d5e3efaf89a57458ea93b
Samuel Malta <samuel.agendaos@gmail.com>
2026-05-21 01:42:15 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
-import os
 from supabase import create_client
+import os
 
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-supabase = create_client(url, key)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
